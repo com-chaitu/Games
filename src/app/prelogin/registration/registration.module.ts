b1490750f64c90696b7eea88c5b339b1b1881382
com-chaitu <ramachaitanya.g@gmail.com>
2018-10-06 18:06:44 -0500

    progress bar initial commit

--- src/app/prelogin/registration/registration.module.ts
+++ src/app/prelogin/registration/registration.module.ts
@@ -6,11 +6,12 @@ import { RegFormComponent } from './reg-form/reg-form.component';
 import { RegReviewComponent } from './reg-review/reg-review.component';
 import { RegConfirmationComponent } from './reg-confirmation/reg-confirmation.component';
 import { RegistrationRouting } from './registration-routing.module';
+import { RegProgressBarComponent } from './reg-progress-bar/reg-progress-bar.component';
 
 @NgModule({
   imports: [
     CommonModule, RegistrationRouting, FormsModule
   ],
-  declarations: [RegFormComponent, RegReviewComponent, RegConfirmationComponent]
+  declarations: [RegFormComponent, RegReviewComponent, RegConfirmationComponent, RegProgressBarComponent]
 })
 export class RegistrationModule { }
