9bd79e7d8a9188da2b8056c712efcb545ca5f882
com-chaitu <ramachaitanya.g@gmail.com>
2018-10-07 23:51:54 -0500

    registration form mobile view initial

--- src/app/prelogin/registration/registration.module.ts
+++ src/app/prelogin/registration/registration.module.ts
@@ -7,11 +7,13 @@ import { RegReviewComponent } from './reg-review/reg-review.component';
 import { RegConfirmationComponent } from './reg-confirmation/reg-confirmation.component';
 import { RegistrationRouting } from './registration-routing.module';
 import { RegProgressBarComponent } from './reg-progress-bar/reg-progress-bar.component';
+import { RegistrationService } from './services/registration.service';
 
 @NgModule({
   imports: [
     CommonModule, RegistrationRouting, FormsModule
   ],
-  declarations: [RegFormComponent, RegReviewComponent, RegConfirmationComponent, RegProgressBarComponent]
+  declarations: [RegFormComponent, RegReviewComponent, RegConfirmationComponent, RegProgressBarComponent],
+  providers:[RegistrationService]
 })
 export class RegistrationModule { }
