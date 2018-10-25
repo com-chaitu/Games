fdda1018fc345a10c637fe2017fcdb37f8c45bd7
com-chaitu <ramachaitanya.g@gmail.com>
2018-10-25 00:15:40 -0500

    field validations

--- src/app/prelogin/registration/reg-form/reg-form.component.ts
+++ src/app/prelogin/registration/reg-form/reg-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, HostListener } from '@angular/core';
 import { BackendService } from '../../../services/backend.service';
 import { RegistrationService } from '../services/registration.service';
 import { RegistrationModel } from '../model/registration.model';
@@ -10,7 +10,68 @@ import { Router, ActivatedRoute } from '@angular/router';
   styleUrls: ['./reg-form.component.css']
 })
 export class RegFormComponent implements OnInit {
+  vaidationRules = {
+    "emailId": {
+      "isMandatory": true,
+      "regex": "^(.+)@(.+)\\.[a-zA-Z]{2,5}$",
+      "allowedRegex": "^(.){0,}$"
+    },
+    "firstName": {
+      "isMandatory": true,
+      "regex": "^[a-zA-Z ]{1,}$",
+      "allowedRegex": "^[a-zA-Z ]{0,40}$"
+    },
+    "lastName": {
+      "isMandatory": false,
+      "regex": "^[a-zA-Z ]{0,}$",
+      "allowedRegex": "^[a-zA-Z ]{0,40}$"
+    },
+    "dob": {
+      "isMandatory": false,
+      "regex": null,
+      "allowedRegex": "^(.){0,}$"
+    },
+    "mobileNumber": {
+      "isMandatory": true,
+      "regex": "^[0-9]{10}$",
+      "allowedRegex": "^[0-9]{0,10}$"
+    },
+    "city": {
+      "isMandatory": false,
+      "regex": "^[a-zA-Z ]{0,}$",
+      "allowedRegex": "^[a-zA-Z ]{0,24}$"
+    },
+    "pincode": {
+      "isMandatory": false,
+      "regex": "^[0-9]{0,6}$",
+      "allowedRegex": "^[0-9]{0,6}$"
+    },
+    "default": {
+      "isMandatory": false,
+      "regex": "^(.){0,}$",
+      "allowedRegex": "^(.){0,}$"
+    },
+  };
+
   registrationData: RegistrationModel;
+  hasEmailError = false;
+  hasMobileError = false;
+  hasFirstNameError = false;
+
+  @ViewChild('regForm')
+  regForm: ElementRef;
+
+  @HostListener('window:keyup', ['$event']) onkeyup(event) {
+    const targetId = event.target.id;
+    const validationRule = this.vaidationRules[targetId];
+    const regex = validationRule ? new RegExp(validationRule.allowedRegex) : new RegExp(this.vaidationRules['default'].allowedRegex);
+    const errorTarget: HTMLElement = this.regForm.nativeElement.querySelector('#' + targetId + 'Error');
+    if (errorTarget) {
+      while (!regex.test(event.target.value)) {
+        event.target.value = event.target.value.slice(0, event.target.value.length - 1);
+      }
+    }
+  }
 
   constructor(
     private _regService: RegistrationService,
@@ -27,8 +88,41 @@ export class RegFormComponent implements OnInit {
     }
   }
 
+  onInputBlur(event) {
+    const targetId = event.target.id;
+    const validationRule = this.vaidationRules[targetId];
+    const regex = validationRule ? new RegExp(validationRule.regex) : new RegExp(this.vaidationRules['default'].regex);
+    const errorTarget: HTMLElement = this.regForm.nativeElement.querySelector('#' + targetId + 'Error');
+    if (errorTarget) {
+      if (!regex.test(event.target.value)) {
+        if (event.target.value) {
+          errorTarget.innerText = 'Please enter valid ' + event.target.placeholder + '.';
+        } else if (validationRule.isMandatory) {
+          errorTarget.innerText = 'Please enter ' + event.target.placeholder + '.';
+        }
+
+        if ('emailId' === targetId) {
+          this.hasEmailError = true;
+        } else if ('firstName' === targetId) {
+          this.hasFirstNameError = true;
+        } else if ('mobileNumber' === targetId) {
+          this.hasMobileError = true;
+        }
+      } else {
+        errorTarget.innerText = '';
+        if ('emailId' === targetId) {
+          this.hasEmailError = false;
+        } else if ('firstName' === targetId) {
+          this.hasFirstNameError = false;
+        } else if ('mobileNumber' === targetId) {
+          this.hasMobileError = false;
+        }
+      }
+    }
+  }
+
   onSubmit() {
     this._regService.registrationData = this.registrationData;
-    this._router.navigate(['../reg-review'], {relativeTo: this._route});
+    this._router.navigate(['../reg-review'], { relativeTo: this._route });
   }
 }
